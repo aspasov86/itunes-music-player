09ad032f367aadf2d06a0eba4812cf86305a2b88
unknown <acko1986@yahoo.com>
2021-02-14 23:14:05 +0100

    audio player added

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -1,30 +1,50 @@
 import PropTypes from 'prop-types';
-import { useMemo, useRef, useEffect } from 'react';
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+const getLargerImage = (src) => {
+  const splitSrc = src.split('/');
+  splitSrc[splitSrc.length - 1] = '550x550bb.jpg';
+  return splitSrc.join('/');
+};
 
 const Track = ({ match, history }) => {
   const tracks = useSelector(store => store.tracks);
   const trackNum = useMemo(() => tracks.findIndex((({ trackId }) => trackId === +match.params.trackId)), [match.params.trackId]);
   const selectedTrack = useMemo(() => tracks[trackNum], [trackNum]);
   const nextTrack = useMemo(() => (!tracks[trackNum + 1] ? tracks[0].trackId : tracks[trackNum + 1].trackId), [trackNum]);
-  const onNext = () => history.push(`/track/${nextTrack}`);
+  const previousTrack = useMemo(
+    () => (!tracks[trackNum - 1] ? tracks[tracks.length - 1].trackId : tracks[trackNum - 1].trackId),
+    [trackNum]
+  );
+  const onClickNext = () => history.push(`/track/${nextTrack}`);
+  const onClickPrevious = () => history.push(`/track/${previousTrack}`);
   const onBack = () => history.push('/search');
-  const audio = useRef(null);
-  useEffect(() => { audio.current.load(); }, [trackNum]);
   return (
     <div>
       <button type="button" onClick={onBack}>Back to Search</button>
       <div>
         <div>{selectedTrack.trackName}</div>
       </div>
-      <div>
-        <audio controls ref={audio} crossOrigin="anonymous" autoPlay>
-          <track kind="captions" />
-          <source src={selectedTrack.previewUrl} type="audio/mp4" />
-          Your browser does not support the audio element.
-        </audio>
+      <div style={{ width: 700 }}>
+        <AudioPlayer
+          header={(
+            <div style={{ textAlign: 'center' }}>
+              <img src={getLargerImage(selectedTrack.artworkUrl100)} alt="cover" />
+            </div>
+          )}
+          src={selectedTrack.previewUrl}
+          volume={0.2}
+          showSkipControls
+          onClickPrevious={onClickPrevious}
+          onClickNext={onClickNext}
+          showJumpControls={false}
+          customAdditionalControls={[]}
+          crossOrigin="anonymous"
+        />
       </div>
-      <button type="button" onClick={onNext}>Next</button>
     </div>
   );
 };
