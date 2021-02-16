177f7e9b30744bfd5beda62476efb8164fa31c60
unknown <acko1986@yahoo.com>
2021-02-16 11:37:56 +0100

    oursorced sass constants

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
-import './index.css';
+import './index.module.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
