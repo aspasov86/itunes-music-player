e2bcbc57ede5060ba953f3bae7a35b9383f91eec
Aleksandar Spasov <>
2021-02-17 12:41:11 +0100

    comment added

--- src/redux/thunk.js
+++ src/redux/thunk.js
@@ -8,6 +8,10 @@ const searchForTracks = searchTerm => async (dispatch) => {
   let error = false;
   try {
     const results = await fetchTracks(searchTerm);
+    // Removing anything other than tracks (audiobooks etc)
+    // as track page is based on the trackIds
+    // Example: audiobooks do not have id
+    // Also preventing track duplicates (there were cases)
     tracks = uniqBy(results.filter(({ trackId }) => trackId), 'trackId');
   } catch (e) {
     error = true;
